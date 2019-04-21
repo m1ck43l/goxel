c90519026bf80177f55429e6fad37a54e0f37c63
Mickael <m1ck43lc@gmail.com>
2019-04-21 20:15:51 +0200

    Improve code

--- goxel/monitor.go
+++ goxel/monitor.go
@@ -47,11 +47,53 @@ func QuietMonitoring(files []*File, done chan bool, d chan download) {
 	}
 }
 
+func buildFileDescription(output []string, files []*File) []string {
+	for idx, f := range files {
+		if f.Error == "" {
+			output = append(output, fmt.Sprintf("[%3d] - %-120v", idx, f.Output))
+		} else {
+			output = append(output, fmt.Sprintf("[ERR] - %v: %v", f.Output, f.Error))
+		}
+	}
+	return output
+}
+
+func buildChunkDescription(output []string, files []*File, count uint64, speed uint64) ([]string, int, uint64) {
+	finished := 0
+	var gDone uint64
+
+	for idx, f := range files {
+		if !f.Valid {
+			continue
+		}
+
+		ratio, conn, done, sdone := f.UpdateStatus(count%10 == 0)
+		if f.Finished {
+			finished++
+		}
+
+		left := fmt.Sprintf("[%3d] - [%6.2f%%] [", idx, ratio)
+
+		var remaining uint64
+		if speed > 0 {
+			remaining = uint64(math.Max(float64(f.Size)-float64(done), 0)) / speed
+		}
+		right := fmt.Sprintf("] (%d/%d) [%8v]", conn, len(f.Chunks), fmtDuration(remaining))
+
+		unit := float64(int(getWidth())-len(left)-len(right)-1) / float64(f.Size)
+		output = append(output, left+f.BuildProgress(unit)+right)
+
+		gDone += sdone
+	}
+
+	return output, finished, gDone
+}
+
 // Monitoring monitors the current downloads and display the speed and progress for each files
 func Monitoring(files []*File, done chan bool, d chan download) {
 	monitors := make([]monitor, monitorCount, monitorCount)
 
-	var count, pDone, gDone uint64
+	var count, pDone uint64
 	var output []string
 
 	lastStart := time.Now()
@@ -60,8 +102,6 @@ func Monitoring(files []*File, done chan bool, d chan download) {
 	for {
 		select {
 		default:
-			gDone = 0
-
 			if goxel.Scroll {
 				output = make([]string, 0)
 			} else {
@@ -70,13 +110,7 @@ func Monitoring(files []*File, done chan bool, d chan download) {
 				output = append(output, fmt.Sprintf(strings.Repeat("\033[F", int(move)))+"\r")
 			}
 
-			for idx, f := range files {
-				if f.Error == "" {
-					output = append(output, fmt.Sprintf("[%3d] - %-120v", idx, f.Output))
-				} else {
-					output = append(output, fmt.Sprintf("[ERR] - %v: %v", f.Output, f.Error))
-				}
-			}
+			output = buildFileDescription(output, files)
 			output = append(output, "")
 
 			var curDone uint64
@@ -92,30 +126,7 @@ func Monitoring(files []*File, done chan bool, d chan download) {
 			output = append(output, fmt.Sprintf("Active connections: %6v", activeConnections.v))
 			output = append(output, "")
 
-			finished := 0
-			for idx, f := range files {
-				if !f.Valid {
-					continue
-				}
-
-				ratio, conn, done, sdone := f.UpdateStatus(count%10 == 0)
-				if f.Finished {
-					finished++
-				}
-
-				left := fmt.Sprintf("[%3d] - [%6.2f%%] [", idx, ratio)
-
-				var remaining uint64
-				if speed > 0 {
-					remaining = uint64(math.Max(float64(f.Size)-float64(done), 0)) / speed
-				}
-				right := fmt.Sprintf("] (%d/%d) [%8v]", conn, len(f.Chunks), fmtDuration(remaining))
-
-				unit := float64(int(getWidth())-len(left)-len(right)-1) / float64(f.Size)
-				output = append(output, left+f.BuildProgress(unit)+right)
-
-				gDone += sdone
-			}
+			output, finished, gDone := buildChunkDescription(output, files, count, speed)
 
 			if finished == len(files) && !closed {
 				close(d)
